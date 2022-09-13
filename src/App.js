5f8886f2a807a67e89244acb42ae0ac3dd6c24c0
Syed Monir <Rambling.ctg@gmail.com>
2022-09-13 14:18:56 +0600

    added fetch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import CounterView from './features/counter/CounterView';
+import PostsView from './features/posts/PostsView';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello There!</h1>
       <CounterView />
+      <PostsView />
     </div>
   );
 }
